0232a045dee1caf08f2196c12624648af0cb402e
arihantb2 <arihantb2@gmail.com>
2015-11-27 00:00:31 -0500

    Collision Avoidance added

--- new_actor.cpp
+++ new_actor.cpp
@@ -30,26 +30,65 @@ sim::actor_command new_actor::act_(sim::world_state& w_state)
 
     sim::actor_command cmd;
 
-    // geometry::point_2d collision_avoid;
-    std::map<guid, sim::actor_state> actors = std::move(w_state.actor_states);
+    geometry::point_2d disp_vector = {0, 0};
+    geometry::point_2d collision_avoid = {0, 0};
+    geometry::point_2d zero= {0, 0};
+    std::map<guid, sim::actor_state> actors = w_state.actor_states;
+    float theta_dev = 0;
+    float dist = 0;
+    float dist_threshold = 0.1;
+    float w_target = 1;
+    float w_collision = -1;
 
-    std::cout << "Id: " << a_state.id << ",\tNeighbors: ";
+    // std::cout << "Agent ID:" << a_state.id << "\t: [" << a_state.pose.position.x << ", " << a_state.pose.position.y << "]\n";
 
-    float dist_to_actor = 0;
     for(auto actor : actors)
     {
         if(actor.first == a_state.id)
             continue;
 
-        // std::cout << actor.first << ", ";
-        // dist_to_actor += eucledian_dist(a_state.pose.position, actor.second.pose.position);
-        dist_to_actor += 1;
+
+        disp_vector.x = actor.second.pose.position.x - a_state.pose.position.x;
+        disp_vector.y = actor.second.pose.position.y - a_state.pose.position.y;
+        // std::cout << "Agent ID:" << actor.first << "\tDisp Vector: [" << disp_vector.x << ", " << disp_vector.y << "]\n";
+        theta_dev = atan2(disp_vector.y, disp_vector.x);
+        dist = eucledian_dist(disp_vector, zero);
+        // std::cout << "dist: " << dist << std::endl;
+        // std::cout << "angle_to_actor: " << theta_dev*180/M_PI << std::endl;
+        if(dist > dist_threshold)
+        {
+            collision_avoid.x += (1/dist)*sin(theta_dev);
+            collision_avoid.y += (1/dist)*cos(theta_dev);
+        }
     }
 
-    std::cout << dist_to_actor << std::endl;
-    cmd.heading_rad = atan2(tc.center.y - a_state.pose.position.y, tc.center.x - a_state.pose.position.x);
-    cmd.velocity_mps = 1.34; //this is typical human walking speed in meters per second;
+    dist = eucledian_dist(collision_avoid, zero);
+    if(dist > dist_threshold)
+    {
+        collision_avoid.x = collision_avoid.x/dist;
+        collision_avoid.y = collision_avoid.y/dist;
+    }
 
+    // std::cout << "collision_avoid: [" << collision_avoid.x << ", " << collision_avoid.y << "]\n\n";
+
+    disp_vector.x = tc.center.x - a_state.pose.position.x;
+    disp_vector.y = tc.center.y - a_state.pose.position.y;
+    dist = eucledian_dist(disp_vector, zero);
+
+    if(dist > dist_threshold)
+    {
+        disp_vector.x = disp_vector.x/dist;
+        disp_vector.y = disp_vector.y/dist;
+    }
+
+    w_target = 1.5;
+    w_collision = -1;
+
+    disp_vector.x = w_target*disp_vector.x + w_collision*collision_avoid.x;
+    disp_vector.y = w_target*disp_vector.y + w_collision*collision_avoid.y;
+
+    cmd.heading_rad = atan2(disp_vector.y, disp_vector.x);
+    cmd.velocity_mps = 1.34; //this is typical human walking speed in meters per second;
     return cmd;
 }
 
